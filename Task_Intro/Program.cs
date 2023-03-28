545206175eb66afee410b77c30a13163caaab637
SamirManafDev <samirmanafli161@gmail.com>
2023-03-29 01:49:13 +0400

    Task BackEnd Introduction

---
+++ Task_Intro/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        ////Task 1.1
+        //int number = 12;
+        //double number1 = 32.2;
+        //float weight = 3.4f;
+        //long distance= 91321311371283l;
+        //decimal money = 12.3021m;
+        //string name = "Samir";
+        //char word = 'A';
+        //bool gende = true;
+
+        ////Task 1.2
+        //int intNumber = 23;
+        //float floatNumber = Convert.ToSingle(intNumber);
+        //Console.WriteLine(floatNumber);
+
+        ////Task 1.3
+        //Console.WriteLine("Input number: ");
+        //string number = Console.ReadLine();
+        //int Number=Convert.ToInt32(number);
+        //if (Number>0)
+        //{
+        //    Console.WriteLine("Number positive");
+        //}
+        //else if(Number<0)
+        //{
+        //    Console.WriteLine("Number negative");
+        //}
+        //else if(Number==0)
+        //{
+        //    Console.WriteLine("Number Zero");
+        //}
+
+        ////Task 1.4
+        //Console.WriteLine("Input age: ");
+        //string age = Console.ReadLine();
+        //int yourAge=Convert.ToInt32(age);
+        //if (yourAge>=18)
+        //{
+        //    Console.WriteLine("You can vote");
+        //}
+        //else if (yourAge < 18)
+        //{
+        //    Console.WriteLine("You can`t vote");
+        //}
+
+        ////Task 1.5
+        //for(int i=1; i<100; i++)
+        //{
+        //    if(i%2==0) {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+        ////Task 1.6
+        //int Number;
+        //Console.Write("Input number:");
+        //Number=int.Parse(Console.ReadLine());
+        //for (int i = 1; i <= 10; i++)
+        //{
+        //    Console.WriteLine(Number + "*" + i + "=" + Number * i);
+        //}
+
+
+
+        ////Task 1.7
+        //string correctPassword = "Code2022!";
+        //Console.WriteLine("Input password");
+        //string Password= Console.ReadLine();
+        //while (correctPassword != Password)
+        //{
+        //    Console.WriteLine("Input correct password!!!");
+        //    Password = Console.ReadLine();
+        //}
+
+    }
+}
